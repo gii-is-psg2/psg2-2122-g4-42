25eb7728374cb720334f72c40466a52729e2acfe
Juan Antonio <61751661+JuanAntonio4@users.noreply.github.com>
2022-03-13 13:41:08 +0100

    fix:Delete an import

--- src/test/java/org/springframework/samples/petclinic/vet/VetServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetServiceTests.java
@@ -29,7 +29,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.util.EntityUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 
 /**
